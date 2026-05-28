7792b134f03845046603babcb640050fe2a6cbad
Marcos <marcosalvarezbaz@gmail.com>
2026-05-28 23:34:10 +0200

    Añadida funcionalidad de division

--- Calculadora.java
+++ Calculadora.java
@@ -1,5 +1,9 @@
 public class Calculadora {
-     public static void main(String[] args) { 
-        System.out.println("Calculadora Iniciada"); 
-        }
+    public static void main(String[] args) {
+        System.out.println("Calculadora Iniciada");
+        
+        // Probando la funcionalidad de la rama Division
+        double resultadoDiv = Division.realizarDivision(10.0, 2.0);
+        System.out.println("El resultado de la division es: " + resultadoDiv);
+    }
 }
---
+++ Division.java
@@ -0,0 +1,5 @@
+public class Division {
+    public static double realizarDivision(double a, double b) {
+        return a / b;
+    }
+}
