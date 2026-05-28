1f657f62fad2d68b592b74cc14f17b719e2b44c0
Marcos <marcosalvarezbaz@gmail.com>
2026-05-28 23:31:41 +0200

    Añadida funcionalidad de multiplicacion

--- Calculadora.java
+++ Calculadora.java
@@ -1,5 +1,9 @@
 public class Calculadora {
-     public static void main(String[] args) { 
-        System.out.println("Calculadora Iniciada"); 
-        }
+    public static void main(String[] args) {
+        System.out.println("Calculadora Iniciada");
+        
+        // Probando la funcionalidad de la rama Multiplicacion
+        double resultadoMulti = Multiplicacion.realizarMultiplicacion(10.5, 2.0);
+        System.out.println("El resultado de la multiplicacion es: " + resultadoMulti);
+    }
 }
